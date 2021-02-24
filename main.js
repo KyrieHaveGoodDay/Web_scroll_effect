388c8ec70d684a31cbee48d29d997cbc4a0338dc
kyriehavegoodday <healthymbear@gmail.com>
2021-02-24 21:31:00 +0800

    滾動效果完成

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ $("a").each(function (index, element) {
     console.log("編號"+index);
     
     var target= $(this).attr("data-st-target");
-    
     var duration= $(this).attr("data-duration");
     console.log("名稱"+target);
     console.log("時間"+duration);
+
+    var offset = $("#" + target).offset();
+    var top = offset.top;
+
+    console.log("上方：" + top);
+    
+    $(this).click(function (e) { 
+        e.preventDefault();
+        $("html").stop().animate({
+            scrollTop:top
+        }, parseInt(duration));
+    });
+    
 });
